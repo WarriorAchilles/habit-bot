972696d639e58317722ea23b12bf33159142453f
Zion Emond <zionem7@gmail.com>
2025-03-18 22:35:38 -0400

    update db utility commands

---
+++ README.md
@@ -0,0 +1,9 @@
+# Habit Bot - WIP
+
+A discord bot for tracking your habits daily.
+
+## Database Setup
+
+To initialize the database on initial setup, run `npm run createDb` to create the sqlite database.
+
+To update the database after modifications have been made to the database models, run `npm run syncDb`
--- syncdb.js
+++ syncdb.js
@@ -1,7 +1,16 @@
 import { db } from './utils/database.js';
 
-// completely re-builds the database, wiping everything
-// db.sync({ force: true });
+// eslint-disable-next-line no-undef
+const args = process.argv.slice(2);
 
-// syncs the database, updating any models that have been changed
-db.sync({ alter: true });
+if (args[0] && args[0] === '--force') {
+    console.log('Rebuilding the database...');
+    // completely re-builds the database, wiping everything
+    db.sync({ force: true });
+} else {
+    console.log('Syncing the database...');
+    // syncs the database, updating any models that have been changed
+    db.sync({ alter: true });
+}
+
+console.log('Finished');
